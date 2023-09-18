2ee6006f9e23a419a4ba042c9e31f576f9763fc0
Regimar <60851078+regimarnegreiros@users.noreply.github.com>
2023-09-18 08:53:40 -0300

    Correção questão 5 prática 3

--- praticas/pratica03/questao05.c
+++ praticas/pratica03/questao05.c
@@ -5,7 +5,7 @@ int main() {
     printf("Insira um ano:\n");
     scanf("%i", &ano);
 
-    if (ano % 4 == 0 || ano % 100 == 0 || ano % 400 == 0) {
+    if ((ano % 4 == 0 && !(ano % 100 == 0)) || ano % 400 == 0) {
         printf("O ano %i é bisexto", ano);
     } else {
         printf("O ano %i não é bisexto", ano);
